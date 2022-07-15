f58aa645d202ad79160fca73a42440d334c4a4af
marck0101 <marcos.henrique.a7x@gmail.com>
2022-07-15 16:29:35 -0300

    primeiros passos na programação orientada a objetos

---
+++ cadastro/script.js
@@ -0,0 +1,14 @@
+class Produto{
+    constructor(){
+
+    }
+    adicionar(){
+        alert('adicinar novo produto'); 
+    }
+
+    excluir(){
+        alert('excluir produto'); 
+    }
+}
+
+var produto = new Produto();
