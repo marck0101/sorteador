a10870d58cdd9ef078167ab0350f3b32ba8cd06e
marck0101 <marcos.henrique.a7x@gmail.com>
2022-07-18 17:07:47 -0300

    construtor POO

--- cadastro/script.js
+++ cadastro/script.js
@@ -1,14 +1,16 @@
-class Produto{
-    constructor(){
+class Produto {
+  constructor() {
+    this.id = 0;
+    this.nomeProduto = "";
+    this.valor = ""
+  }
+  adicionar() {
+    alert("adicinar novo produto");
+  }
 
-    }
-    adicionar(){
-        alert('adicinar novo produto'); 
-    }
-
-    excluir(){
-        alert('excluir produto'); 
-    }
+  excluir() {
+    alert("excluir produto");
+  }
 }
 
 var produto = new Produto();
