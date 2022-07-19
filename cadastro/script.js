d1a6ea986989a8de0949ba7023a91a88da82c69f
marck0101 <marcos.henrique.a7x@gmail.com>
2022-07-19 17:22:59 -0300

    POO aula 3 iniciada

--- cadastro/script.js
+++ cadastro/script.js
@@ -1,11 +1,26 @@
 class Produto {
   constructor() {
     this.id = 0;
-    this.nomeProduto = "";
-    this.valor = ""
+    this.arrayProdutos = [];
+    // this.nomeProduto = "";
+    // this.valor = "";
   }
   adicionar() {
     alert("adicinar novo produto");
+  }
+
+  salvar() {
+    this.lerDados();
+    console.log(produto)
+  }
+
+  lerDados() {
+    let produto = {}
+
+    produto.id = this.id;
+    produto.nomeProduto = document.getElementById('produto').value
+    produto.preco = document.getElementById('preco').value
+    return produto;
   }
 
   excluir() {
